3bade3e76e11a6502e30ef74e1ce540764803f79
SW/Andy <li.ck8819@gmail.com>
2024-11-07 04:36:15 +0800

    creatte front end ui protopype

--- src/app.py
+++ src/app.py
@@ -1,31 +1,80 @@
-from flask import Flask, request, render_template
-import cv2
-import numpy as np
-import json
-import matplotlib.pyplot as plt
-from Models import LuggageProcessor as lp
+from flask import (
+    Flask,
+    request,
+    redirect,
+    url_for,
+    render_template,
+    send_from_directory,
+)
+import os
+import subprocess
 
 app = Flask(__name__)
-model = lp.LuggageProcessor()
+UPLOAD_FOLDER = "upload"
+RESULT_FOLDER = "Models/ResultImages"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["RESULT_FOLDER"] = RESULT_FOLDER
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+if not os.path.exists(RESULT_FOLDER):
+    os.makedirs(RESULT_FOLDER)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def upload_form():
+    return render_template("upload.html")
 
 
 @app.route("/upload", methods=["POST"])
-def upload_image():
+def upload_file():
     if "file" not in request.files:
-        return "No file part"
+        return redirect(request.url)
     file = request.files["file"]
     if file.filename == "":
-        return "No selected file"
+        return redirect(request.url)
     if file:
-        image_path = f"uploads/{file.filename}"
-        file.save(image_path)
-        model.depth_test(image_path=image_path)
-        return "Image processed successfully"
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+        file.save(file_path)
+
+        # 呼叫 main.py 並傳遞圖片路徑
+        subprocess.run(["python", "main.py", file_path])
+
+        # 假設結果圖片的名稱是 depth_output_原始圖片名稱.png
+        result_image_path = f"depth_output_{file.filename}.png"
+
+        return redirect(url_for("display_result", filename=result_image_path))
+
+
+@app.route("/waiting", methods=["POST"])
+def waiting():
+    if "file" not in request.files:
+        return redirect(url_for("upload_form"))
+    file = request.files["file"]
+    if file.filename == "":
+        return redirect(url_for("upload_form"))
+    if file:
+        file_path = os.path.join(app.config["UPLOAD_FOLDER"], file.filename)
+        file.save(file_path)
+
+        # 呼叫 main.py 並傳遞圖片路徑
+        subprocess.run(["python", "main.py", file_path])
+
+        # 假設結果圖片的名稱是 depth_output_原始圖片名稱.png
+        result_image_path = f"depth_output_{file.filename}.png"
+
+        return redirect(url_for("display_result", filename=result_image_path))
+
+
+@app.route("/result/<filename>")
+def display_result(filename):
+    return render_template("result.html", filename=filename)
+
+
+@app.route("/uploads/<filename>")
+def send_file(filename):
+    return send_from_directory(app.config["RESULT_FOLDER"], filename)
 
 
 if __name__ == "__main__":
